b3ecb021d6d988b673bf4b1edd955761f801039f
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-11 00:06:20 +0000

    Subo el audio y vista del titiritero

---
+++ src/titiritero/vista/MouseClickController.java
@@ -0,0 +1,21 @@
+package titiritero.vista;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import titiritero.ControladorJuego;
+
+
+public class MouseClickController extends MouseAdapter {
+
+	private ControladorJuego controlador; 
+	
+	public MouseClickController(ControladorJuego unControlador) {
+		this.controlador = unControlador;
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		this.controlador.despacharMouseClick(e.getX(), e.getY());
+	}
+	
+}
