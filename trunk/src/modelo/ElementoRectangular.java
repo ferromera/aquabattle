67f0d66112cbffdab554649f26df103d1570d3fe
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-11 01:38:00 +0000

    Agrego ElementoRectangular

---
+++ trunk/src/modelo/ElementoRectangular.java
@@ -0,0 +1,95 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import misc.Observable;
+import misc.Observador;
+import titiritero.Posicionable;
+
+
+public class ElementoRectangular implements Posicionable , Observable {
+	private int posX;
+	private int posY;
+	private int alto;
+	private int ancho;
+	private ArrayList<Observador> observadores;
+	
+	public ElementoRectangular(){
+		posX=0;
+		posY=0;
+		alto=30;
+		ancho=30;
+		observadores=new ArrayList<Observador>();
+	}
+	public ElementoRectangular(int x,int y){
+		posX=x;
+		posY=y;
+		alto=30;
+		ancho=30;
+		observadores=new ArrayList<Observador>();
+	}
+	public ElementoRectangular(int x,int y,int alto, int ancho){
+		posX=x;
+		posY=y;
+		this.alto=alto;
+		this.ancho=ancho;
+		observadores=new ArrayList<Observador>();
+	}
+	public int getX(){
+		return posX;
+	}
+	public int getY(){
+		return posY;
+	}
+	public void setX(int x){
+		posX=x;
+	}
+	public void setY(int y){
+		posY=y;
+	}
+	public int getAlto(){
+		return alto;
+	}
+	public int getAncho(){
+		return ancho;
+	}
+	public void setAlto(int alto){
+		this.alto=alto;
+	}
+	public void setAncho(int ancho){
+		this.ancho=ancho;
+	}
+	public void adscribir(Observador observador){
+		if(!observadores.contains(observador))
+		observadores.add(observador);
+	}
+	public void quitar(Observador observador){
+		observadores.remove(observador);
+	}
+	public void notificar(){
+		Iterator<Observador> it= observadores.iterator();
+		while(it.hasNext()){
+			it.next().actualizar();
+		}
+		
+	}
+	public boolean superpuestoCon(ElementoRectangular rect){
+		if( posX <= rect.posX + rect.ancho &&
+			posY <= rect.posY + rect.alto &&
+			posX + ancho >= rect.posX &&
+			posY + alto >= rect.posY )
+			return true;
+		return false;
+	}
+	public boolean fueraDeEscenario(){
+		/* FALTA ESCENARIO
+		escenario= Escenario.getInstance();
+		 
+		if( posX < 0 || posX >= escenario.getAncho() ||
+			posY < 0 || posY >= escenario.getAlto() )
+			return true;
+		 */
+		return false;
+	}
+}
