f98ae7938a104e325a90067a8f105b17a0af199f
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-11 00:57:12 +0000

    Agrego comentarios

--- trunk/src/misc/Observable.java
+++ trunk/src/misc/Observable.java
@@ -2,7 +2,21 @@ package misc;
 
 
 public interface Observable {
+	/*
+	 * adscribir: debe agregar obs a una 
+	 * coleccion de observadores asociados con
+	 * el objeto observable.
+	 */
 	public void adscribir(Observador obs);
+	/*
+	 * quitar: debe remover obs de la 
+	 * coleccion de observadores asociados con
+	 * el objeto observable.
+	 */
 	public void quitar(Observador obs);
+	/*
+	 * notificar: debe actualizar todos los observadores
+	 * asociados con el objeto observable.
+	 */
 	public void notificar();
 }
--- trunk/src/misc/Observador.java
+++ trunk/src/misc/Observador.java
@@ -1,5 +1,10 @@
 package misc;
 
 public interface Observador {
+	/*
+	 * actualizar: debe actualizar el estado
+	 * del observador basandose en el estado
+	 * del objeto observado.
+	 */
 	public void actualizar();
 }
