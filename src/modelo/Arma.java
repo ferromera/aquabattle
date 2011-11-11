5bcdbc5e5bb076054ac9c20b6af2bc6ec024c1a1
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-11 19:13:22 +0000

    Puse una lista de armas para que el tanque si agarra un arma pueda seguir usando las que tenia antes.

--- src/modelo/Arma.java
+++ src/modelo/Arma.java
@@ -1,5 +1,10 @@
 package modelo;
 
+
+
 public abstract class Arma {
 	public abstract void disparar();
+	public boolean equals(Arma a){
+		return (getClass().equals(a.getClass()));
+	}
 }
--- src/modelo/Tanque.java
+++ src/modelo/Tanque.java
@@ -1,53 +1,112 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import excepciones.NoExisteArmaSeleccionadaException;
+
 import titiritero.ObjetoVivo;
 
-public abstract class Tanque extends ElementoRectangularSolido implements ObjetoVivo{
-	private Arma arma;
+public abstract class Tanque extends ElementoRectangularSolido implements
+		ObjetoVivo {
+	private ArrayList<Arma> armas;
+	private Arma armaActual;
+	private Iterator<Arma> itArmaActual;
 	private int vida;
 	private boolean moviendose;
-	
-	public Tanque(Arma arma){
-		
-		moviendose=false;
+
+	public Tanque() {
+
+		moviendose = false;
 		orientarNorte();
-		vida=100;
-		this.arma=arma;
+		vida = 100;
+		armas = new ArrayList<Arma>();
+		itArmaActual = armas.iterator();
+		this.armaActual = null;
 	}
+
 	public abstract void vivir();
-	public void disparar(){
-		arma.disparar();
+
+	public void disparar() {
+		armaActual.disparar();
 	}
-	public void moverNorte(){
+
+	public void moverNorte() {
 		orientarNorte();
-		moviendose=true;
+		moviendose = true;
 	}
-	public void moverSur(){
+
+	public void moverSur() {
 		orientarNorte();
-		moviendose=true;
+		moviendose = true;
 	}
-	public void moverOeste(){
+
+	public void moverOeste() {
 		orientarNorte();
-		moviendose=true;
+		moviendose = true;
 	}
-	public void moverEste(){
+
+	public void moverEste() {
 		orientarNorte();
-		moviendose=true;
+		moviendose = true;
 	}
-	public void detener(){
-		moviendose=false;
+
+	public void detener() {
+		moviendose = false;
 	}
-	protected void setVida(int vida){
-		this.vida=vida;
+
+	protected void setVida(int vida) {
+		this.vida = vida;
 	}
-	public int getVida(){
+
+	public int getVida() {
 		return vida;
 	}
-	public boolean enMovimiento(){
+
+	public boolean enMovimiento() {
 		return moviendose;
 	}
-	public void cambiarArma(Arma armaNueva){
-		arma=armaNueva;
+
+	public void siguienteArma() {
+		if (itArmaActual.hasNext())
+			armaActual = itArmaActual.next();
+		else {
+			itArmaActual = armas.iterator();
+			if (itArmaActual.hasNext())
+				armaActual = itArmaActual.next();
+			else
+				armaActual = null;
+
+		}
 	}
-	
+
+	public void agregarArma(Arma arma) {
+		armas.remove(arma);
+		armas.add(arma);
+		try {
+			seleccionarArma(arma);
+		} catch (NoExisteArmaSeleccionadaException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void seleccionarArma(Arma arma)
+			throws NoExisteArmaSeleccionadaException {
+
+		itArmaActual = armas.iterator();
+		while (itArmaActual.hasNext()) {
+			armaActual = itArmaActual.next();
+			if (armaActual.equals(arma))
+				return;
+		}
+		throw new NoExisteArmaSeleccionadaException();
+	}
+
+	public void quitarArma() {
+		armas.remove(armaActual);
+		itArmaActual=armas.iterator();
+		siguienteArma();
+	}
+
 }
