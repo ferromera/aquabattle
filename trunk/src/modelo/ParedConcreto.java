82ecf0a1a5d3b9d99e3b7faa2631e42c35d64fbf
juani2687kun@gmail.com <juani2687kun@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-12 16:12:30 +0000

    Agregada clase ParedConcreto y modificada clase Pared

--- trunk/src/modelo/ParedConcreto.java
+++ trunk/src/modelo/ParedConcreto.java
@@ -6,25 +6,27 @@ import titiritero.Posicionable;
 import misc.Observable;
 import misc.Observador;
 
-public abstract class Pared extends ElementoRectangularSolido implements 
+public abstract class ParedConcreto extends Pared implements 
 ObjetoVivo,Posicionable, Observable {
 	
-	int impactosRecibidos;
-	
 	private final int ALTO = 20;
-	private final int ANCHO = 20;
+	private final int ANCHO = 20;	
 	
-	public Pared(int posicionEnX, int posicionEnY){
+	public ParedConcreto(int posicionEnX, int posicionEnY){
 		setX(posicionEnX);
 		setY(posicionEnY);
 		setAlto(ALTO);      //Poner la medida que se va a usar
 		setAncho(ANCHO); 
 		this.impactosRecibidos = 0;
 	}
-
 	
-	public abstract void recibirImpacto(int fuerza);
 	
-
+	public void recibirImpacto(int fuerza){
+		this.impactosRecibidos ++;
+		if(impactosRecibidos > 0){
+			Escenario escenarioActual = Escenario.getActual();
+			escenarioActual.borrarSolido(this);
+		}
+	}
 	
 }
