78c0278f1f14a2c5ff61b2bc72270196304f7ec0
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-12 13:44:10 +0000

    Agrego la fabrica de elementos

---
+++ trunk/src/modelo/FabricaElementos.java
@@ -0,0 +1,28 @@
+package modelo;
+
+import modelo.armamento.BalaAmetralladora;
+import modelo.armamento.BalaCanion;
+import modelo.armamento.Cohete;
+
+public class FabricaElementos {
+
+	
+	public static BalaAmetralladora crearBalaAmetralladora(){
+		BalaAmetralladora bala = new BalaAmetralladora();
+		Escenario.getActual().agregarObjetoSolido(bala);
+		Escenario.getActual().agregarObjetoVivo(bala);
+		return bala;
+	}
+	public static BalaCanion crearBalaCanion(){
+		BalaCanion bala = new BalaCanion();
+		Escenario.getActual().agregarObjetoSolido(bala);
+		Escenario.getActual().agregarObjetoVivo(bala);
+		return bala;
+	}
+	public static Cohete crearCohete(){
+		Cohete cohete = new Cohete();
+		Escenario.getActual().agregarObjetoSolido(cohete);
+		Escenario.getActual().agregarObjetoVivo(cohete);
+		return cohete;
+	}
+}
