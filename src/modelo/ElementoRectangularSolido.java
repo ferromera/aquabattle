0a74a2047cc10fac235b4f45d8bbbf23c8870dbb
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-12 00:10:28 +0000

    Movi el chequeo de colision a ElementoSolido

--- src/modelo/ElementoRectangularSolido.java
+++ src/modelo/ElementoRectangularSolido.java
@@ -1,8 +1,38 @@
 package modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import excepciones.NoExisteElementoColisionadoException;
+
 /*
  * Representa todos los elementos del modelo que pueden chocar
  * entre ellos.
  */
 public abstract class ElementoRectangularSolido extends ElementoRectangular implements Impactable {
 	public abstract void recibirImpacto(int fuerza);
+	
+	private ElementoRectangularSolido elemColisionado=null;
+	
+	public ElementoRectangularSolido getColisionado() throws NoExisteElementoColisionadoException{
+		if(!estaColisionado())
+			throw new NoExisteElementoColisionadoException();
+		return elemColisionado;
+	}
+	
+	public boolean estaColisionado(){
+		Escenario escenario = Escenario.getActual();
+		ArrayList<ElementoRectangularSolido> solidos= escenario.getSolidos();
+		Iterator<ElementoRectangularSolido> it = solidos.iterator();
+		while(it.hasNext()){
+			ElementoRectangularSolido solido= it.next();
+			if( solido.equals(this))
+				continue;
+			if(superpuestoCon(solido)){
+				elemColisionado=solido;
+				return true;
+			}
+		}
+		return false;
+	}
 }
