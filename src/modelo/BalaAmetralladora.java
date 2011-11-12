1eebf5bb9d893ae26d24fc75556b040ad10dc538
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-12 02:59:06 +0000

    Hice las tres balas, puse en impactable el metodo getResistencia ya que lo necesitaba para impactar scando la mitad de resistencia.

--- src/modelo/BalaAmetralladora.java
+++ src/modelo/BalaAmetralladora.java
@@ -1,49 +1,14 @@
 package modelo;
 
 
-import java.util.Date;
-
-
-import excepciones.NoExisteElementoColisionadoException;
 
 public class BalaAmetralladora extends Bala {
-	private long ultimoTiempo;
 	private final int FUERZA=20;
-	private final double VELOCIDAD= 150;
+	private final double VELOCIDAD=150.0;
+	
 	public BalaAmetralladora(){
-		ultimoTiempo=new Date().getTime();
-	}
-	public void vivir(){
-		long tiempoActual=new Date().getTime();
-		int intervaloTiempo=(int)(tiempoActual-ultimoTiempo);
-		ultimoTiempo=tiempoActual;
-		double movimientoRestante=(VELOCIDAD*(double)intervaloTiempo/1000.0);
-
-		while(movimientoRestante > 1.0){
-			movimientoRestante--;
-			avanzar();
-			if(estaColisionado()){
-				try {
-					impactar(getColisionado());
-				} catch (NoExisteElementoColisionadoException e) {
-					e.printStackTrace();
-				}
-				return;
-			}
-		}
-		avanzar(movimientoRestante);
-		if(estaColisionado()){
-			try {
-				impactar(getColisionado());
-			} catch (NoExisteElementoColisionadoException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	public void impactar(ElementoRectangularSolido solido){
-		if(solido != null)
-			solido.recibirImpacto(FUERZA);
-		destruir();
+		velocidad=VELOCIDAD;
+		fuerza=FUERZA;
 	}
 	
 
