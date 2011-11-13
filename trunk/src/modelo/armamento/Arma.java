318f9a962f29d67c71a441edd4bd9bbe10b9d41f
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-13 13:52:30 +0000

    Agrego pruebas de armas.

--- trunk/src/modelo/armamento/Arma.java
+++ trunk/src/modelo/armamento/Arma.java
@@ -13,7 +13,7 @@ import utils.Direccion;
 
 public abstract class Arma implements ActionListener  {
 	private Tanque tanque;
-	private boolean cargada;
+	private boolean cargada=true;
 	protected int tiempoCarga;
 	protected int cantidadDeBalas;
 	
@@ -39,29 +39,33 @@ public abstract class Arma implements ActionListener  {
 			destruir();
 		
 	}
-	public void posicionarBala(Bala bala){
+	private void posicionarBala(Bala bala){
 		double xBala= tanque.getX();
 		double yBala= tanque.getY();
 		switch(tanque.getOrientacion().get()){
 		case Direccion.NORTE:
 				yBala-= bala.getAlto() + 1;
 				//centrada en X
-				xBala+= (tanque.getAncho()-bala.getAncho())/2; 
+				xBala+= (tanque.getAncho()-bala.getAncho())/2;
+				bala.setOrientacion(Direccion.Norte());
 				break;
 		case Direccion.SUR:
 				yBala+= tanque.getAlto() + 1;
 				//centrada en X
 				xBala+= (tanque.getAncho()-bala.getAncho())/2; 
+				bala.setOrientacion(Direccion.Sur());
 				break;
 		case Direccion.ESTE:
 				xBala+= tanque.getAncho() + 1;
 				//centrada en Y
 				yBala+= (tanque.getAlto()-bala.getAlto())/2; 
+				bala.setOrientacion(Direccion.Este());
 				break;
 		case Direccion.OESTE:
 				xBala-= bala.getAncho() + 1;
 				//centrada en Y
 				yBala+= (tanque.getAlto()-bala.getAlto())/2;
+				bala.setOrientacion(Direccion.Oeste());
 				break;
 		}
 		bala.setX(xBala);
@@ -71,11 +75,11 @@ public abstract class Arma implements ActionListener  {
 	 * Debe crear la instancia de bala correspondiente
 	 * y actualizar la municion del arma.
 	 */
-	public abstract Bala crearBala();
+	protected abstract Bala crearBala();
 	
 	public abstract boolean tieneMunicion();
 	
-	public void destruir(){
+	private void destruir(){
 		if(tanque!=null)
 			tanque.quitarArma();
 		tanque=null;
@@ -90,6 +94,7 @@ public abstract class Arma implements ActionListener  {
 	}
 	public void setTanque(Tanque tanque){
 		this.tanque=tanque;
+		tanque.agregarArma(this);
 	}
 	public Tanque getTanque(){
 		return tanque;
