19d3b207d2a1ddc9754525428987b3ca4beaa9a0
juani2687kun@gmail.com <juani2687kun@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-16 19:39:26 +0000

    Modificados los bonus

--- src/modelo/BonusAtaque.java
+++ src/modelo/BonusAtaque.java
@@ -2,7 +2,10 @@ package modelo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
+
+import modelo.mejoras.MejoraTanqueAtaque;
+
+import excepciones.NoSePudoPosicionarException;
 
 import titiritero.ObjetoVivo;
 import titiritero.Posicionable;
@@ -11,31 +14,23 @@ public class BonusAtaque extends ElementoRectangularIntangible implements
 ObjetoVivo, Posicionable, ActionListener {
 	
 	public BonusAtaque(){
-		setX(0.0);
-		setY(0.0);
-	}
-	
-	public BonusAtaque(double posicionEnX, double posicionEnY){
-		setX(posicionEnX);
-		setY(posicionEnY);
-	}
-	
-	public void vivir(){
-		boolean estaEnContacto = false;
-		Escenario escenario = Escenario.getActual();
-		Iterator<ElementoRectangular> it = escenario.getObjetos();
-		ElementoRectangular elem;
-		
-		while(!estaEnContacto && it.hasNext()){
-			elem = it.next();
-			if ((this.superpuestoCon(elem)) && (elem instanceof Tanque)) {
-				estaEnContacto = true;
-				((Tanque)elem).mejorarVelocidad(20.0);
-				((Tanque)elem).mejorarVelocidadDisparo(30.0);
-				Escenario.getActual().borrarObjeto(this);
-			}
+		try{
+		PosicionadorAleatorioStd posicionAleatoria = new PosicionadorAleatorioStd(this);
+		posicionAleatoria.posicionar();
+		}catch (NoSePudoPosicionarException e){
 		}
 	}
+	
+	
+	public void vivir(){
+		TanqueHeroe tanque = TanqueHeroe.getInstancia();
+		
+		if(this.superpuestoCon(tanque)){
+			MejoraTanqueAtaque mejoraAtaque = new MejoraTanqueAtaque();
+			tanque.agregarMejora(mejoraAtaque);
+			Escenario.getActual().borrarObjeto(this);
+			}
+		}
 	
 	public void actionPerformed(ActionEvent e){
 		Escenario.getActual().borrarObjeto(this);
