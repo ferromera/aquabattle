57bf7c0e9b7f6486a2f61d882aefff61d09a81f0
canuhedc@gmail.com <canuhedc@gmail.com@2481ef78-1a56-a436-0cf6-d7a0397f2b4d>
2011-11-16 14:04:44 +0000

    tanques enemigos (falta implementar ai)

---
+++ trunk/src/modelo/ai/BotBordes.java
@@ -0,0 +1,24 @@
+package modelo.ai;
+
+import modelo.ElementoRectangular;
+import modelo.Tanque;
+
+public class BotBordes extends Bot {
+	
+	public BotBordes(Tanque tanque, ElementoRectangular objetivo) {
+		super(tanque, objetivo);
+	}
+
+	@Override
+	void decidirDisparo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void calcularOrientacion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
